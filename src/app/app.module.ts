41e0f866e41e9ea339fcb7a934a6c37adf69f280
Guilherme Oliveira <guglieelmor@gmail.com>
2023-04-16 05:30:43 -0300

    release incomplete for the middle of site

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,21 +6,21 @@ import { AppComponent } from './app.component';
 
 import { HeaderSiteComponent } from './components/header/header-site/header-site.component';
 import { FooterSiteComponent } from './components/footer/footer-site/footer-site.component';
-import { LoginComponent } from './pages/login/login.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { SiteComponent } from './pages/site/site.component';
 import { FirstSiteComponent } from './components/section/first-site/first-site.component';
 import { LoaderSiteComponent } from './components/section/loader-site/loader-site.component';
+import { AboutComponent } from './pages/site/about/about.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderSiteComponent,
     FooterSiteComponent,
-    LoginComponent,
     SiteComponent,
     FirstSiteComponent,
-    LoaderSiteComponent
+    LoaderSiteComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
