1d9066c609b09909a020d1708e2ff59a6569baea
Guilherme Oliveira <guglieelmor@gmail.com>
2023-04-21 23:20:20 -0300

    integration sendgrid email

--- src/app/pages/site/contact/contact.component.ts
+++ src/app/pages/site/contact/contact.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { EmailService } from 'src/app/services/email.service';
+ 
 
 @Component({
   selector: 'app-contact',
@@ -6,5 +8,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./contact.component.scss']
 })
 export class ContactComponent {
+  constructor(private emailService: EmailService) {}
 
+  sendEmail() {
+    const to = 'contato@guilmor.com';
+    const from = 'guilmor.softwares@gmail.com';
+    const subject = 'Assunto do email';
+    const message = 'Corpo do email';
+
+    this.emailService.sendEmail(to, from, subject, message)
+      .then(() => {
+        console.log('Email enviado com sucesso!');
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }
 }
