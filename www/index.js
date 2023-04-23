b077e075406cd8bb7b5f2388225242bf771f4936
Guilherme Oliveira <guglieelmor@gmail.com>
2023-04-23 19:06:08 -0300

    policy CORS api mail / app

--- www/index.js
+++ www/index.js
@@ -4,6 +4,14 @@ const bodyParser = require('body-parser');
 const winston    = require('winston');
 
 const app = express();
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:4200'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 app.use(bodyParser.json());
 
 sgMail.setApiKey('');
