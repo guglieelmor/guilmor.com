f1d68bba48810b7da3f77458b39d541814051fbe
Guilherme Oliveira <guglieelmor@gmail.com>
2023-04-30 01:43:54 -0300

    settings API

--- www/index.js
+++ www/index.js
@@ -2,17 +2,19 @@ const express    = require('express');
 const sgMail     = require('@sendgrid/mail');
 const bodyParser = require('body-parser');
 const winston    = require('winston');
-const cors       = require('cors');
 
 const app = express();
 
-app.use(cors({
-  origin: 'https://guilmor.com'
-}));
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'https://guilmor.com'); 
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
 
 app.use(bodyParser.json());
 
-sgMail.setApiKey('');
+sgMail.setApiKey('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');
 
 const Log = winston.createLogger({
   level: 'info',
@@ -47,6 +49,6 @@ app.post('/send', async (req, res) => {
   }
 });
 
-app.listen(2000, '0.0.0.0', () => {
+app.listen(2000, () => {
   console.log('Server starting with port 2000.');
 });
