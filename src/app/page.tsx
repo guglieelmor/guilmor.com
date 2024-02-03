f71f34c3d65a7acb240468aaae672ed503d0383b
Gulliver <guglieelmor@gmail.com>
2024-02-03 19:55:44 -0300

    navbar complete

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <> 
       <Navbar />
-      <Header />
-      <Footer /> 
+      {/* <Header /> */}
+      {/* <Footer />  */}
     </>
   );
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,4 +1,5 @@
 "use client";
+
 import {
   DropdownMenu,
   DropdownMenuContent,
@@ -7,10 +8,10 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger,
 } from "@/components/ui/dropdown-menu";
-
 import Link from "next/link";
 import Image from "next/image";
 import { Button } from "./ui/button";
+import { CiMenuFries } from "react-icons/ci";
 
 export default function Navbar() {
   return (
@@ -18,8 +19,8 @@ export default function Navbar() {
       <div className="g-gradient h-2"></div>
       <div className="container flex">
         <div className="flex-1">
-          <div className="h-auto">
-            <Link className="hover:opacity-75 w-0 h-0" href="/">
+          <div className="flex">
+            <Link className="hover:opacity-75 h-auto" href="/">
               <Image
                 src="/assets/logotipo/logo.png"
                 alt="GUILMOR: Soluções em Tecnologia da Informação"
@@ -35,21 +36,7 @@ export default function Navbar() {
           <DropdownMenuTrigger>
             <div className="m-auto inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600">
               <span className="sr-only">Abrir menu</span>
-              <svg
-                className="w-5 h-5"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="none"
-                viewBox="0 0 17 14"
-              >
-                <path
-                  stroke="currentColor"
-                  stroke-linecap="round"
-                  stroke-linejoin="round"
-                  stroke-width="2"
-                  d="M1 1h15M1 7h15M1 13h15"
-                />
-              </svg>
+              <CiMenuFries size={20} className="m-auto text-white" />
             </div>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
